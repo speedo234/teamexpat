f576899edbf8c55979fe6c308b874f321781795a
Isidienu Chudi <talk2speedy@yahoo.com>
2022-07-25 19:35:17 +0100

    initial commit...

---
+++ src/main/java/com/teamexpat/TeamexpatApplication.java
@@ -0,0 +1,18 @@
+package com.teamexpat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamexpatApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TeamexpatApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamexpatApplication.class, args);
+		LOGGER.info("Application Fired Up and Running...");
+	}
+
+}
---
+++ src/test/java/com/teamexpat/TeamexpatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamexpat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamexpatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

56a58ca21d4b42cbc3dd036be7da35dc87be3896
Isidienu Chudi <talk2speedy@yahoo.com>
2022-07-26 23:12:21 +0100

    code clean up...

--- src/main/java/com/teamexpat/TeamexpatApplication.java
+++ src/main/java/com/teamexpat/TeamexpatApplication.java
@@ -12,7 +12,6 @@ public class TeamexpatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeamexpatApplication.class, args);
-		LOGGER.info("Application Fired Up and Running...");
 	}
 
 }

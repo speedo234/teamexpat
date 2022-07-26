5c6a76297b76227dc9bffef279c3452a87f7a006
Isidienu Chudi <talk2speedy@yahoo.com>
2022-07-26 22:21:02 +0100

    implemented unit tests...

---
+++ src/test/java/com/teamexpat/service/impl/FileServiceImplTest.java
@@ -0,0 +1,75 @@
+package com.teamexpat.service.impl;
+
+import org.apache.commons.csv.CSVRecord;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
+@ExtendWith(MockitoExtension.class)
+class FileServiceImplTest {
+
+
+    @InjectMocks
+    FileServiceImpl fileService;
+
+    @Value("${input.file.directory}")
+    private String inputDirectoryString;
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void convertStringToFile() {
+
+        String inputDirectoryString = "C:/Users/ibren/gitrepositories/java projects/teamexpat/src/main/resources/array.csv";
+
+        File actual = fileService.convertStringToFile(inputDirectoryString);
+
+        assertThat(actual).isFile();
+
+    }
+
+    @Test
+    void convertFileToMultipartFile() throws IOException {
+
+        String inputDirectoryString = "C:/Users/ibren/gitrepositories/java projects/teamexpat/src/main/resources/array.csv";
+
+        File file = fileService.convertStringToFile(inputDirectoryString);
+
+        MultipartFile actual = fileService.convertFileToMultipartFile(file);
+
+        assertThat(actual).isInstanceOf(MultipartFile.class);
+
+    }
+
+    @Test
+    void getCSVRecordsFromInputFile() throws IOException {
+
+        String inputDirectoryString = "C:/Users/ibren/gitrepositories/java projects/teamexpat/src/main/resources/array.csv";
+
+        File file = fileService.convertStringToFile(inputDirectoryString);
+
+        MultipartFile multipartFile = fileService.convertFileToMultipartFile(file);
+
+        List<CSVRecord> actual = fileService.getCSVRecordsFromInputFile(multipartFile);
+
+        assertThat(actual.size()).isGreaterThan(0);
+    }
+}
